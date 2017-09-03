2e6121e7e6e034ca3076ec40d4be231d0e1ac96a
Naoki Takezoe <takezoe@gmail.com>
2017-09-03 21:38:24 +0900

    Fix mounting path of controller

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -8,6 +8,6 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val versions: List[Version] = List(new Version("1.0.0"))
 
   override val controllers = Seq(
-    "/helloworld" -> new HelloWorldController()
+    "/*" -> new HelloWorldController()
   )
 }
